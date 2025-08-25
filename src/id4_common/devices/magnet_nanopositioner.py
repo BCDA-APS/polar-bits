81086aeba0623acee01139db82b138f6b30a91ed
gfabbris <gfabbris@anl.gov>
2025-08-25 13:03:38 -0500

    Copy files from old repository

---
+++ src/id4_common/devices/magnet_nanopositioner.py
@@ -0,0 +1,11 @@
+"""
+Magnet Nanopositioner motors
+"""
+
+from ophyd import Component, MotorBundle, EpicsMotor
+
+
+class NanoPositioner(MotorBundle):
+    x = Component(EpicsMotor, "m1", labels=("motor",))
+    y = Component(EpicsMotor, "m2", labels=("motor",))
+    z = Component(EpicsMotor, "m3", labels=("motor",))
